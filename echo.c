ccbb7dbc6cdc980997ec7a54c100c1e136de0878
shiori-42 <shiori0123.happysmile@gmail.com>
2024-11-05 18:55:30 +0900

    echoコマンドの実装

--- echo.c
+++ echo.c
@@ -1,20 +1,48 @@
 #include "minishell.h"
 
+// void builtin_echo(char **args) {
+//     int i = 1;
+//     int newline = 1;  // デフォルトでは改行あり
+
+//     // `-n` オプションが最初に指定されている場合の処理
+//     if (args[1] && strcmp(args[1], "-n") == 0) {
+//         newline = 0;
+//         i = 2;  // 次の引数から出力開始
+//     }
+
+//     // 引数を順に出力
+//     for (; args[i]; i++) {
+//         printf("%s", args[i]);
+//         if (args[i + 1]) {
+//             printf(" ");
+//         }
+//     }
+
+//     // 改行の有無
+//     if (newline) {
+//         printf("\n");
+//     }
+// }
+
+#include <stdio.h>
+#include <string.h>
+
+// echo コマンドの実装
 void builtin_echo(char **args) {
     int i = 1;
-    int newline = 1;  // デフォルトでは改行あり
+    int newline = 1; // 改行を出力するかどうか（1 = 出力する, 0 = 出力しない）
 
     // `-n` オプションが最初に指定されている場合の処理
     if (args[1] && strcmp(args[1], "-n") == 0) {
-        newline = 0;
-        i = 2;  // 次の引数から出力開始
+        newline = 0; // 改行を出力しない
+        i = 2;       // 次の引数から出力開始
     }
 
     // 引数を順に出力
     for (; args[i]; i++) {
         printf("%s", args[i]);
         if (args[i + 1]) {
-            printf(" ");
+            printf(" "); // 引数の間にスペースを入れる
         }
     }
 
@@ -23,3 +51,6 @@ void builtin_echo(char **args) {
         printf("\n");
     }
 }
+
+
+
