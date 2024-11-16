77a5b07defe39ef520b9c57e67d5bcf716afb4cb
shiori-42 <shiori0123.happysmile@gmail.com>
2024-11-16 09:47:14 +0000

    入力リダイレクトの実装

--- redirect.c
+++ redirect.c
@@ -15,28 +15,41 @@ int handle_redirection(char **command) {
     int redirect_fd;
     // 標準出力の上書きリダイレクト ">"
     for (int i = 0; command[i] != NULL; i++) {
-        if ((strcmp(command[i], ">") == 0 || strcmp(command[i], ">>") == 0)  && command[i + 1] != NULL) {
+        if (((strcmp(command[i], ">") == 0 || strcmp(command[i], ">>") == 0) || strcmp(command[i], "<") == 0) && command[i + 1] != NULL) {
             if (strcmp(command[i], ">") == 0) 
             {
                 redirect_fd = open(command[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
             }
-            else
+            else if (strcmp(command[i], ">>") == 0)
             {
                 redirect_fd = open(command[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+            }
+            else if (strcmp(command[i], "<") == 0)
+            {
+                redirect_fd = open(command[i + 1], O_RDONLY);
             }
             if (redirect_fd == -1) {
                 perror(command[i + 1]); 
                 return -1;
             }
-
-            // 標準出力をリダイレクト
-            if (dup2(redirect_fd, STDOUT_FILENO) == -1) {
-                perror("dup2");
+            if (strcmp(command[i], ">") == 0 || strcmp(command[i], ">>") == 0)
+            {   // 標準出力をリダイレクト
+                if (dup2(redirect_fd, STDOUT_FILENO) == -1) {
+                    perror("dup2");
+                    close(redirect_fd);
+                    return -1;
+                }
                 close(redirect_fd);
-                return -1;
             }
-            close(redirect_fd);
-
+            else if (strcmp(command[i], "<") == 0)
+            {   // 標準入力をリダイレクト
+                if (dup2(redirect_fd, STDIN_FILENO) == -1) {
+                    perror("dup2");
+                    close(redirect_fd);
+                    return -1;
+                }
+                close(redirect_fd);
+            }
             // リダイレクト演算子とファイル名をコマンドから削除
             free(command[i]);
             free(command[i + 1]);
