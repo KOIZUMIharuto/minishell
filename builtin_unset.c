195458fb17051e15826fc7d115e091695054694a
KOIZUMIharuto <105192235+KOIZUMIharuto@users.noreply.github.com>
2025-03-17 15:08:47 +0900

    fix: norm対応 purser*,env*,builtin*(exit除く)

--- builtin_unset.c
+++ builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: hkoizumi <hkoizumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 18:11:10 by shiori            #+#    #+#             */
-/*   Updated: 2025/03/17 14:33:38 by hkoizumi         ###   ########.fr       */
+/*   Updated: 2025/03/17 15:07:16 by hkoizumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	builtin_unset(char **cmd, t_list *env)
 	while (cmd[i])
 	{
 		if (!is_valid_key(cmd[i]) || !env_delete(&env, cmd[i]))
-				status = 1;
+			status = 1;
 		i++;
 	}
 	return (status);
--- purser_redirect.c
+++ purser_redirect.c
@@ -12,26 +12,26 @@
 
 #include <minishell.h>
 
-static t_rdrct	*set_rdrct(char **key_pos, char *key, t_purser *data);
-static int		set_rdrct_type(char *key_pos, char key, t_rdrct *rdrct);
-static bool		set_file(char *key_pos, t_rdrct *rdrct, int len, t_purser *data);
+static t_rdrct	*set_rdrct(char **key_p, char *key, t_purser *data);
+static int		set_rdrct_type(char *key_p, char key, t_rdrct *rdrct);
+static bool		set_file(char *key_p, t_rdrct *rdrct, int len, t_purser *data);
 
 t_rdrct	**check_rdrct(char *line, char *key, int rdrct_cnt, t_purser *data)
 {
 	t_rdrct	**rdrcts;
 	t_rdrct	*rdrct;
-	char	*key_pos;
+	char	*key_p;
 
 	if (!line)
 		return (NULL);
-	key_pos = line;
-	while (*key_pos && !is_del(*key_pos, key, NULL))
-		key_pos++;
-	if (!*key_pos)
+	key_p = line;
+	while (*key_p && !is_del(*key_p, key, NULL))
+		key_p++;
+	if (!*key_p)
 		rdrcts = (t_rdrct **)ft_calloc(rdrct_cnt + 1, sizeof(t_rdrct *));
 	else
 	{
-		rdrct = set_rdrct(&key_pos, key, data);
+		rdrct = set_rdrct(&key_p, key, data);
 		if (!rdrct)
 			return (NULL);
 		rdrcts = check_rdrct(line, key, rdrct_cnt + 1, data);
@@ -43,7 +43,7 @@ t_rdrct	**check_rdrct(char *line, char *key, int rdrct_cnt, t_purser *data)
 	return (rdrcts);
 }
 
-static t_rdrct	*set_rdrct(char **key_pos, char *key, t_purser *data)
+static t_rdrct	*set_rdrct(char **key_p, char *key, t_purser *data)
 {
 	t_rdrct	*rdrct;
 	t_quote	quote;
@@ -53,29 +53,29 @@ static t_rdrct	*set_rdrct(char **key_pos, char *key, t_purser *data)
 	rdrct = (t_rdrct *)ft_calloc(1, sizeof(t_rdrct));
 	if (!rdrct)
 		return (NULL);
-	key_pos_tmp = *key_pos;
-	*key_pos += set_rdrct_type(*key_pos, key[0], rdrct);
-	while (**key_pos && is_del(**key_pos, " \t\n", NULL))
-		(*key_pos)++;
+	key_pos_tmp = *key_p;
+	*key_p += set_rdrct_type(*key_p, key[0], rdrct);
+	while (**key_p && is_del(**key_p, " \t\n", NULL))
+		(*key_p)++;
 	quote = NONE_Q;
 	i = 0;
-	while ((*key_pos)[i] && !is_del((*key_pos)[i], " \t\n><", &quote))
+	while ((*key_p)[i] && !is_del((*key_p)[i], " \t\n><", &quote))
 		i++;
-	if (!set_file(*key_pos, rdrct, i, data))
+	if (!set_file(*key_p, rdrct, i, data))
 	{
 		free_rdrct (rdrct);
 		return (NULL);
 	}
 	rdrct->fd = -1;
-	(void)ft_strlcpy(key_pos_tmp, *key_pos + i, ft_strlen(key_pos_tmp) + 1);
+	(void)ft_strlcpy(key_pos_tmp, *key_p + i, ft_strlen(key_pos_tmp) + 1);
 	return (rdrct);
 }
 
-static int	set_rdrct_type(char *key_pos, char key, t_rdrct *rdrct)
+static int	set_rdrct_type(char *key_p, char key, t_rdrct *rdrct)
 {
 	if (key == '<')
 	{
-		if (key_pos[1] == '<')
+		if (key_p[1] == '<')
 		{
 			rdrct->type = HEREDOCUMENT;
 			return (2);
@@ -85,7 +85,7 @@ static int	set_rdrct_type(char *key_pos, char key, t_rdrct *rdrct)
 	}
 	else
 	{
-		if (key_pos[1] == '>')
+		if (key_p[1] == '>')
 		{
 			rdrct->type = APPEND_RDRCT;
 			return (2);
@@ -96,21 +96,21 @@ static int	set_rdrct_type(char *key_pos, char key, t_rdrct *rdrct)
 	return (1);
 }
 
-static bool	set_file(char *key_pos, t_rdrct *rdrct, int len, t_purser *data)
+static bool	set_file(char *key_p, t_rdrct *rdrct, int len, t_purser *data)
 {
 	char	*file_name_tmp;
 
-	if (key_pos[0])
+	if (key_p[0])
 	{
 		if (rdrct->type == HEREDOCUMENT)
 		{
 			rdrct->file = (char **)ft_calloc(2, sizeof(char *));
 			if (rdrct->file)
-				rdrct->file[0] = ft_substr(key_pos, 0, len);
+				rdrct->file[0] = ft_substr(key_p, 0, len);
 		}
 		else
 		{
-			file_name_tmp = ft_substr(key_pos, 0, len);
+			file_name_tmp = ft_substr(key_p, 0, len);
 			if (!file_name_tmp)
 				return (false);
 			rdrct->file = split_arg(file_name_tmp, data);
